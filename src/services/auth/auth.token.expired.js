372d90c9a17746fcb6e7c4ee2f33e9747a8a9565
MarouaneSaoud <marouanesaoud84@gmail.com>
2023-07-25 18:01:19 +0200

    EXPIRED

---
+++ src/services/auth/auth.token.expired.js
@@ -0,0 +1,15 @@
+// tokenUtils.js
+import jwt_decode from 'jwt-decode';
+
+ const isTokenExpired=() => {
+    try {
+        const token  = localStorage.getItem('accessToken');
+        const decodedToken = jwt_decode(token);
+        const expirationTime = decodedToken.exp * 1000; // Convertir le timestamp UNIX en millisecondes
+        const currentTime = Date.now();
+        return expirationTime < currentTime;
+    } catch (error) {
+        return true;
+    }
+};
+ export default isTokenExpired();
