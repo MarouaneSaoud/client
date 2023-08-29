e84392978768dd68f6f7115f995c06eb2c8f7bdc
MarouaneSaoud <marouanesaoud84@gmail.com>
2023-08-29 11:57:27 +0200

    commit

--- src/services/statistics.service.js
+++ src/services/statistics.service.js
@@ -3,7 +3,7 @@ import http from "./http.common.js";
 import getAuthHeaders from "./auth/auth.header";
 const headers = getAuthHeaders();
 async function getStatistics() {
-    return await http.get("/statistics/last-nine-months",{headers});
+    return await http.get("/statistics/statisticAdmin",{headers});
 }
 
 const Statistics = {getStatistics}
